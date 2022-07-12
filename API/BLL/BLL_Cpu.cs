7b9ac412c0739dd46e50bc6961014f5f089766dd
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-12 12:19:05 +0100

    Optimisation and increasing scraping accuracy

--- API/BLL/BLL_Cpu.cs
+++ API/BLL/BLL_Cpu.cs
@@ -5,13 +5,38 @@ namespace API.BLL;
 
 public class BLL_Cpu
 {
+    public static string Reverse(string str)
+    {
+        char[] ch = str.ToCharArray();
+        Array.Reverse(ch);
+        return new string(ch);
+    }
+
     public object GetInfoFromCpu(string isbn)
     {
         IWebDriver driver;
         driver = new ChromeDriver();
+        IJavaScriptExecutor js = (IJavaScriptExecutor) driver;
         try
         {
-            //todo: need to increse accuracy of scraping
+            isbn = isbn.Replace(" ", "");
+            if (isbn.All(char.IsDigit))
+            {
+                if (isbn.Length < 13)
+                {
+                    var isbnError = new {error = "ISBN is not valid"};
+                    return isbnError;
+                }
+
+                if (isbn.Length == 13)
+                {
+                    isbn = isbn.Insert(3, "-");
+                    isbn = isbn.Insert(8, "-");
+                    isbn = isbn.Insert(11, "-");
+                    isbn = isbn.Insert(15, "-");
+                }
+            }
+
 
             driver.Navigate().GoToUrl("https://www.cpu.rnu.tn/index.php/shop/");
             var input = driver.FindElement(
@@ -25,12 +50,25 @@ public class BLL_Cpu
 
             System.Threading.Thread.Sleep(5000);
             var cover = driver.FindElement(By.ClassName("attachment-shop_single")).GetAttribute("src");
-            var ISBN = driver.FindElement(By.XPath("//div[1]/div[2]/div[2]/div/p[3]")).Text;
-            ISBN = ISBN.Substring(7);
-            var title = driver.FindElement(By.XPath("//div[1]/div[2]/div[1]/h1")).Text;
+            var ISBN = js.ExecuteScript("const ps = document.querySelectorAll('p');" +
+                                        "for (var i = 0; i < ps.length; i++) {" +
+                                        "if (ps[i].textContent.includes('ت.د.م.ك')) {" +
+                                        "return ps[i].textContent" +
+                                        "}" +
+                                        "if(ps[i].textContent.includes('ISBN'))return(ps[i].textContent);" +
+                                        "}");
 
-            var editorString = driver.FindElement(By.XPath("//div[1]/div[2]/div[2]/div/p[2]")).Text;
-            var authors = editorString.Split(new[] {",", "et"}, StringSplitOptions.RemoveEmptyEntries);
+            if (ISBN.ToString().Contains("ت.د.م.ك"))
+            {
+                ISBN = ISBN.ToString().Replace("ت.د.م.ك. : ", "");
+                ISBN = Reverse(ISBN.ToString());
+            }
+
+            ISBN = ISBN.ToString().Replace("ISBN : ", "");
+            var title = driver.FindElement(By.XPath("//div[1]/div[2]/div[1]/h1")).Text;
+            var editorString = js.ExecuteScript(
+                "return (document.querySelector('div.cmsmasters_single_product > div.summary.entry-summary.cmsmasters_product_right_column > div.cmsmasters_product_content > div > p > strong') != null ?  (document.querySelector('div.cmsmasters_single_product > div.summary.entry-summary.cmsmasters_product_right_column > div.cmsmasters_product_content > div > p > strong').textContent.includes(':') ? document.querySelector('div.cmsmasters_single_product > div.summary.entry-summary.cmsmasters_product_right_column > div.cmsmasters_product_content > div > p:nth-child(2) > strong').textContent : document.querySelector('div.cmsmasters_single_product > div.summary.entry-summary.cmsmasters_product_right_column > div.cmsmasters_product_content > div > p > strong').textContent) : document.querySelector('div.cmsmasters_single_product > div.summary.entry-summary.cmsmasters_product_right_column > div.cmsmasters_product_content > div > p:nth-child(2)').textContent)");
+            var authors = editorString.ToString().Split(new[] {",", "et"}, StringSplitOptions.RemoveEmptyEntries);
             var result = new {cover, title, ISBN, authors};
             driver.Quit();
             return result;

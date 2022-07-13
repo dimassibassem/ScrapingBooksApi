914a80fc20886eee4a03512030531409a7dc7118
Bassem Dimassi <dimassibassem99@gmail.com>
2022-07-13 10:06:25 +0100

    It works but with many issues from BNT source

--- API/BLL/BLL_BNT.cs
+++ API/BLL/BLL_BNT.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace API.BLL;
@@ -10,24 +11,36 @@ public class BLL_BNT
     {
         IWebDriver driver;
         ChromeOptions options = new ChromeOptions();
-        // options.AddArgument("--headless");
+        options.AddArgument("--headless");
         driver = new ChromeDriver(options);
+        driver.Navigate().GoToUrl("https://www.bnt.nat.tn");
+        Thread.Sleep(2000);
+        var input = driver.FindElement(By.Id("searchdata1"));
+        input.SendKeys(isbn);
+        Thread.Sleep(2000);
+        var dropdown = driver.FindElement(By.Id("srchfield1"));
+        var selectElement = new SelectElement(dropdown);
+        selectElement.SelectByText("ISBN");
+        Thread.Sleep(1000);
+        var button =
+            driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[1]/div[1]/div/form[1]/div/div[2]/input"));
+        button.Click();
+        Thread.Sleep(2000);
+
         try
         {
-            driver.Navigate().GoToUrl("https://www.bnt.nat.tn");
+            driver.FindElement(By.XPath("//*[@id='hitlist']/ul/li[3]/ul[1]/li[3]/dl/dd[1]/a")).Click();
             Thread.Sleep(2000);
-            var input = driver.FindElement(By.Id("searchdata1"));
-            input.SendKeys(isbn);
-            Thread.Sleep(2000);
-            var dropdown = driver.FindElement(By.Id("srchfield1"));
-            var selectElement = new SelectElement(dropdown);
-            selectElement.SelectByText("ISBN");
-            Thread.Sleep(1000);
-            var button = driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[1]/div[1]/div/form[1]/div/div[2]/input"));
-            button.Click();
-            Thread.Sleep(2000);
-            
-            var cover = driver.FindElement(By.XPath("/html/body/div[5]/div[1]/form[1]/div[2]/div[1]/ul[1]/img")).GetAttribute("src");
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+
+        try
+        {
+            var cover = driver.FindElement(By.XPath("/html/body/div[5]/div[1]/form[1]/div[2]/div[1]/ul[1]/img"))
+                .GetAttribute("src");
             var ISBN = driver.FindElement(By.ClassName("isbn")).Text;
             var authors = driver.FindElement(By.ClassName("author")).Text;
             var title = driver
@@ -38,6 +51,60 @@ public class BLL_BNT
         }
         catch (Exception e)
         {
+            driver.Quit();
+            var error = new {error = "Book not found"};
+            return error;
+        }
+    }
+
+    public object GetInfoFromBNTWithTitle(string typedTitle)
+    {
+        IWebDriver driver;
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl("https://www.bnt.nat.tn");
+        Thread.Sleep(2000);
+        var input = driver.FindElement(By.Id("searchdata1"));
+        input.SendKeys(typedTitle);
+        Thread.Sleep(2000);
+        var dropdown = driver.FindElement(By.Id("srchfield1"));
+        var selectElement = new SelectElement(dropdown);
+        selectElement.SelectByText("Titre");
+        Thread.Sleep(1000);
+        var button =
+            driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[1]/div[1]/div/form[1]/div/div[2]/input"));
+        button.Click();
+        Thread.Sleep(2000);
+
+        try
+        {
+            driver.FindElement(By.XPath("//*[@id='hitlist']/ul/li[3]/ul[1]/li[3]/dl/dd[1]/a")).Click();
+            Thread.Sleep(2000);
+        }
+        catch (Exception e)
+        {
+            // ignored
+        }
+
+        try
+        {
+            var cover = driver.FindElement(By.XPath("/html/body/div[5]/div[1]/form[1]/div[2]/div[1]/ul[1]/img"))
+                .GetAttribute("src");
+            var ISBN = driver.FindElement(By.ClassName("isbn")) != null
+                ? driver.FindElement(By.ClassName("isbn")).Text
+                : "";
+            var authors = driver.FindElement(By.ClassName("author")).Text;
+            var title = driver
+                .FindElement(By.ClassName("title")).Text;
+            var result = new {cover, title, ISBN, authors};
+            driver.Quit();
+            return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
             driver.Quit();
             var error = new {error = "Book not found"};
             return error;
--- API/Controllers/BNT.cs
+++ API/Controllers/BNT.cs
@@ -1,6 +1,16 @@
 using API.BLL;
 using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
+
+// I don't recommend using this controller
+// the "BNT" website used in this controller
+// is not the best source of information 
+// I find out that the search in the website is not very accurate 
+// and it have a lot of errors in the search
+// especially in the search with ISBN number
+
+
 {
     [Route("api/[controller]/{*isbn}")]
     [ApiController]
@@ -11,6 +21,19 @@ namespace API.Controllers
         {
             BLL_BNT bll = new BLL_BNT();
             var infos = bll.GetInfoFromBNT(isbn);
+            return Ok(infos);
+        }
+    }
+
+    [Route("api/BNT/title/{*title}")]
+    [ApiController]
+    public class BNT_with_title : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Get(string title)
+        {
+            BLL_BNT bll = new BLL_BNT();
+            var infos = bll.GetInfoFromBNTWithTitle(title);
             return Ok(infos);
         }
     }

1397a6b8ab1ebd0d77bf2f4d294972a1836227fa
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-10 13:19:15 +0100

    some Refactoring

--- API/Models/DAL/DBConnection.cs
+++ API/Models/DAL/DBConnection.cs
@@ -4,12 +4,12 @@ namespace API.Models.DAL;
 
 public static class DbConnection
 {
-    static string DbConnnectionString = $"Data Source=localhost;Initial Catalog=books;User ID=SA;Password=yourStrong(!)Password";
+    private const string DbConnectionString = $"Data Source=localhost;Initial Catalog=books;User ID=SA;Password=yourStrong(!)Password";
 
 
     public static SqlConnection GetConnection()
     {
-        return new SqlConnection(DbConnnectionString);
+        return new SqlConnection(DbConnectionString);
     }
     
 }

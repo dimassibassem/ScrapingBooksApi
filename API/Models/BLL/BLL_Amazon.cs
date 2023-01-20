9424e2a5d602f598e15c87bde79b9b760aa433c5
Bassem Dimassi <dimassibassem99@gmail.com>
2023-01-20 12:49:57 +0100

    Amazon Fix getting Book cover

--- API/Models/BLL/BLL_Amazon.cs
+++ API/Models/BLL/BLL_Amazon.cs
@@ -8,7 +8,7 @@ public static class BllAmazon
     public static object GetInfoFromAmazon(string isbn)
     {
         ChromeOptions options = new ChromeOptions();
-        options.AddArgument("--headless");
+        // options.AddArgument("--headless");
         IWebDriver driver = new ChromeDriver(options);
         IJavaScriptExecutor js = (IJavaScriptExecutor) driver;
         try
@@ -16,42 +16,46 @@ public static class BllAmazon
             driver.Navigate().GoToUrl("https://www.amazon.ca/s?k=+" + isbn + "&crid=3P70C7NM9WVF7&sprefix=" + isbn +
                                       "%2Caps%2C190&ref=nb_sb_noss");
             var button = driver.FindElement(By.XPath(
-                "//*[@id='search']/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div[1]/span/a"));
+                "//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/div/div/div[1]/span/a"));
             Thread.Sleep(3000);
             button.Click();
             Thread.Sleep(5000);
 
-            var cover = driver
-                .FindElement(By.XPath(
-                    "//*[@id='imgBlkFront']"))
-                .GetAttribute("src");
+            // var cover = driver
+            //     .FindElement(By.XPath(
+            //         "//*[@id='ebooksImgBlkFront']"))
+            //     .GetAttribute("src");
+            var cover = js.ExecuteScript(
+                "let cover = ''; " +
+                "document.getElementById('ebooksImgBlkFront') == null " +
+                "? " + "cover = document.getElementById('imgBlkFront') " +
+                ": cover = document.getElementById('ebooksImgBlkFront') ;" +
+                " return cover.src;");
+
             var title = driver.FindElement(By.XPath("//*[@id='productTitle']")).Text;
             var editorsElement = driver
                 .FindElements(By.ClassName("_about-the-author-card_carouselItemStyles_authorName__HSb1t"));
-            var editor = new List<string>();
-            foreach (var elem in editorsElement)
-            {
-                editor.Add(elem.Text);
-            }
+            var editor = editorsElement.Select(elem => elem.Text).ToList();
 
             var ISBN = js.ExecuteScript("const div = document.querySelector('#detailBullets_feature_div > ul');" +
                                         "const lis = div.querySelectorAll('ul>li');" +
                                         "for (let i = 0; i < lis.length; i++)" +
-                                        " {if(lis[i].querySelector('li>span>span').textContent.includes('ISBN-13')) return(lis[i].querySelector('li> span > span:nth-child(2)').textContent);}");
-
+                                        " {if(lis[i].querySelector('li>span>span').textContent.includes('ISBN-13')) return(lis[i].querySelector('li> span > span:nth-child(2)').textContent);}") ??
+                       isbn;
             Thread.Sleep(5000);
             var result = new {cover, title, ISBN, editor};
             driver.Quit();
             return result;
         }
-        catch (Exception)
+        catch (Exception e)
         {
             var error = new
             {
                 error = "Book not found"
             };
             driver.Quit();
-            return error;
+            // return error;
+            return e.ToString();
         }
     }
 }
--- API/Models/DBConnection.cs
+++ API/Models/DBConnection.cs
@@ -4,7 +4,7 @@ namespace API.Models;
 
 public static class DbConnection
 {
-    private const string DbConnectionString = "Data Source=localhost;Initial Catalog=books;User ID=SA;Password=yourStrong(!)Password";
+    private const string DbConnectionString = "Data Source=localhost;Initial Catalog=books;User ID=SA;Password=msSQL2022";
 
 
     public static SqlConnection GetConnection()
